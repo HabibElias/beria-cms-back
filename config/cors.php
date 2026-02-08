ae9da3ed9ad5acd30e613ebc6f4fe2eab7ce8cac
Habib Elias <149260750+HabibElias@users.noreply.github.com>
2026-02-08 13:10:36 -0900

    Add new allowed origin for CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -7,6 +7,7 @@ return [
         'http://127.0.0.1:5173',
         'http://localhost:5174',
         'http://127.0.0.1:5174',
+        'https://berea-cms.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
